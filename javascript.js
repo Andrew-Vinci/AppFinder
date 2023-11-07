bcd634db7a1d50bf09f6a1373f0246879afb7c64
Andrew Vinci <andrewvinci92@gmail.com>
2023-11-07 00:47:26 -0500

    Added basic frontend for skeleton of project.

---
+++ javascript.js
@@ -0,0 +1,19 @@
+
+function createAppIcons(numberOfIcons) {
+    const grid = document.querySelector('.container');
+    counter = 0;
+    for(let i = 0; i < numberOfIcons; i++){
+        const appIcon = document.createElement('div');
+
+        appIcon.textContent = "Item" + counter
+        counter++
+
+        grid.appendChild(appIcon);
+
+    }
+}
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    // Your code to manipulate the DOM goes here
+    createAppIcons(35);
+  });
